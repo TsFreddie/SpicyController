8cf28a76e75e624770ee343053e3ec3477eda792
Freddie Kins <tsfreddiewang@gmail.com>
2021-10-24 02:57:13 +0800

    first commit

---
+++ input.js
@@ -0,0 +1,215 @@
+// ----------------
+//      GLOBAL
+// ----------------
+const api = new SpiceAPI();
+const controller = {
+  data: null,
+};
+const controls = [];
+
+// ----------------
+//     HELPERS
+// ----------------
+
+function save(key, value) {
+  window.localStorage.setItem(key, value);
+}
+
+function load(key) {
+  return window.localStorage.getItem(key);
+}
+
+function show(id) {
+  document.getElementById(id).style.display = 'inherit';
+}
+
+function hide(id) {
+  document.getElementById(id).style.display = 'none';
+}
+
+// ----------------
+//     CONTROLS
+// ----------------
+
+class Button {
+  constructor(root, x, y, width, height, btname) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.btname = btname;
+    this.element = document.createElement('div');
+    this.element.className = 'controllerButton';
+    this.element.style.left = x + '%';
+    this.element.style.top = y + '%';
+    this.element.style.width = width + '%';
+    this.element.style.height = height + '%';
+
+    this.element.addEventListener('pointerdown', () => {
+      api.buttons.write([this.btname, 1]);
+    });
+    this.element.addEventListener('pointerup', () => {
+      api.buttons.write([this.btname, 0]);
+    });
+    root.appendChild(this.element);
+  }
+
+  destroy() {
+    api.buttons.reset(this.btname);
+    root.removeChild(this.element);
+  }
+}
+
+function setControls(data, api) {
+  controller.data = data;
+  const width = data.width || 100;
+  const height = data.height || 100;
+  const offsetX = data.offsetX || 0;
+  const offsetY = data.offsetY || 0;
+
+  updateController();
+
+  const root = document.getElementById('controller');
+  for (const control of data.controls || []) {
+    controls.push(
+      new Button(
+        root,
+        ((control.x + offsetX) / width) * 100,
+        ((control.y + offsetY) / height) * 100,
+        (control.width / width) * 100,
+        (control.height / height) * 100,
+        control.name
+      )
+    );
+  }
+}
+
+function updateController() {
+  if (!controller.data) return;
+
+  const data = controller.data;
+
+  const root = document.getElementById('controller');
+
+  const width = data.width || 100;
+  const height = data.height || 100;
+
+  if (window.innerHeight / window.innerWidth < height / width) {
+    const percentWidth = (window.innerHeight / height / window.innerWidth) * width * 100;
+    root.style.width = `calc(${percentWidth}%)`;
+    root.style.paddingBottom = `calc(${height / width} * ${percentWidth}%)`;
+  } else {
+    root.style.width = 'calc(100%)';
+    root.style.paddingBottom = `calc(${height / width} * 100%)`;
+  }
+}
+
+function clearControls() {
+  controller.data = null;
+  for (const control of controls) {
+    control.destroy();
+  }
+  controls.splice(0, controls.length);
+}
+
+// ----------------
+//  CONNECT WINDOW
+// ----------------
+
+function disconnect() {
+  api.close();
+  const enable = elem => {
+    elem.disabled = false;
+  };
+
+  const address = document.getElementById('address');
+  const port = document.getElementById('port');
+  const password = document.getElementById('password');
+
+  enable(address);
+  enable(port);
+  enable(password);
+  enable(document.getElementById('connect'));
+
+  show('connectWin');
+  hide('controller');
+}
+
+function connect() {
+  const disable = elem => {
+    elem.disabled = true;
+  };
+
+  const address = document.getElementById('address');
+  const port = document.getElementById('port');
+  const password = document.getElementById('password');
+
+  disable(address);
+  disable(port);
+  disable(password);
+  disable(document.getElementById('connect'));
+
+  api.onopen = () => {
+    hide('connectWin');
+    showControlWindow();
+  };
+
+  api.onerror = e => {
+    console.error(e);
+    disconnect();
+  };
+
+  save('address', address.value);
+  save('port', port.value);
+  api.connect(address.value, parseInt(port.value), password.value);
+}
+
+// -------------------
+//  CONTROLLER WINDOW
+// -------------------
+
+function showControlWindow() {
+  clearControls();
+  show('controllerWin');
+  hide('controller');
+}
+
+function hideControlWindow() {
+  hide('controllerWin');
+}
+
+function customControl() {
+  console.log(document.getElementById('code'));
+  try {
+    const text = document.getElementById('code').value;
+    const data = JSON.parse(text);
+    setControls(data);
+    save('custom', text);
+    hideControlWindow();
+    show('controller');
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+// ----------------
+//       MAIN
+// ----------------
+
+let root = document.getElementById('content');
+
+window.addEventListener('load', () => {
+  root = document.getElementById('content');
+
+  const address = load('address');
+  const port = load('port');
+  if (address) document.getElementById('address').value = address;
+  if (port) document.getElementById('port').value = port;
+
+  const custom = load('custom');
+  if (custom) document.getElementById('code').value = custom;
+});
+
+window.addEventListener('resize', () => {
+  updateController();
+});
---
+++ spiceapi.js
@@ -0,0 +1,220 @@
+const SpiceAPI = (() => {
+  const BUFFER_SIZE = 1024 * 8;
+
+  // ----------------
+  //   API WRAPPERS
+  // ----------------
+  class Buttons {
+    constructor(spice) {
+      this.spice = spice;
+    }
+
+    write(button, state) {
+      if (Array.isArray(button) && state == null) {
+        this.spice.request('buttons', 'write', button);
+      } else {
+        this.spice.request('buttons', 'write', [button, state]);
+      }
+    }
+
+    async read() {
+      return this.spice.request('buttons', 'read');
+    }
+
+    reset() {
+      this.spice.request('buttons', 'write_reset');
+    }
+  }
+
+  // ----------------
+  //    ENCRYPTION
+  // ----------------
+  class RC4 {
+    constructor(pass) {
+      this.a = 0;
+      this.b = 0;
+      this.key = new TextEncoder('utf-8').encode(pass);
+      this.box = new Uint8Array(256);
+
+      for (let i = 0; i < 256; i++) {
+        this.box[i] = i;
+      }
+
+      let j = 0;
+      for (let i = 0; i < 256; i++) {
+        j = (j + this.box[i] + this.key[i % this.key.byteLength]) % 256;
+
+        const tmp = this.box[i];
+        this.box[i] = this.box[j];
+        this.box[j] = tmp;
+      }
+    }
+
+    crypt(inData) {
+      for (let i = 0; i < inData.length; i++) {
+        this.a = (this.a + 1) % 256;
+        this.b = (this.b + this.box[this.a]) % 256;
+
+        const tmp = this.box[this.a];
+        this.box[this.a] = this.box[this.b];
+        this.box[this.b] = tmp;
+
+        inData[i] ^= this.box[(this.box[this.a] + this.box[this.b]) % 256];
+      }
+    }
+  }
+
+  // ----------------
+  //       API
+  // ----------------
+  class SpiceAPI {
+    constructor() {
+      this.dataBuffer = new Uint8Array(BUFFER_SIZE);
+      this.dataLen = 0;
+
+      this.lastId = 0;
+      this.callbacks = {};
+
+      this.onopen = null;
+      this.onerror = null;
+
+      // Setup APIs
+      this.buttons = new Buttons(this);
+    }
+
+    connect(host, port, pass) {
+      if (pass != null) {
+        this.pass = new RC4(pass);
+      } else {
+        this.pass = null;
+      }
+      console.log('connecting');
+      try {
+        this.ws = new WebSocket(`ws://${host}:${port + 1}`);
+        this.ws.onopen = async () => {
+          console.log('spice api connected');
+
+          const timeout = setTimeout(() => {
+            this.ws.close();
+            console.log('spice api timed out');
+            if (this.onerror) this.onerror('timeout');
+          }, 1000);
+
+          const res = await this.requestWithID(
+            Math.floor(Math.random() * 2147483648),
+            'control',
+            'session_refresh'
+          );
+          clearTimeout(timeout);
+          console.log(res[0]);
+          this.changePass(res);
+
+          if (this.onopen) {
+            this.onopen();
+          }
+        };
+        this.ws.onmessage = msg => {
+          const reader = new FileReader();
+          reader.readAsArrayBuffer(msg.data);
+          reader.onload = () => {
+            const content = reader.result;
+            if (content instanceof ArrayBuffer) {
+              if (this.dataLen + content.byteLength >= BUFFER_SIZE) {
+                this.close();
+                return;
+              }
+
+              const data = new Uint8Array(content);
+              if (this.pass) {
+                this.pass.crypt(data);
+              }
+
+              this.dataBuffer.set(data, this.dataLen);
+
+              this.dataLen += content.byteLength;
+
+              for (let i = 0; i < this.dataLen; i++) {
+                if (this.dataBuffer[i] === 0) {
+                  const msg = this.dataBuffer.subarray(0, i);
+
+                  if (msg.length > 0) {
+                    // convert msg to JSON
+                    try {
+                      const json = new TextDecoder('utf-8').decode(msg);
+                      const obj = JSON.parse(json);
+                      if (this.callbacks[obj.id]) {
+                        this.callbacks[obj.id](obj.data, obj.errors);
+                        delete this.callbacks[obj.id];
+                      }
+                    } catch (e) {
+                      console.error(e);
+                    }
+                  }
+
+                  // remove range [0, i) from dataBuffer
+                  this.dataBuffer.set(this.dataBuffer.subarray(i + 1, this.dataLen));
+                  this.dataLen = this.dataLen - i - 1;
+                }
+              }
+            }
+          };
+        };
+        this.ws.onclose = e => {
+          console.log('connection closed');
+        };
+
+        this.ws.onerror = e => {
+          console.log('ws error: ' + e);
+          if (this.onerror) {
+            this.onerror(e);
+          }
+          this.ws.close();
+        };
+      } catch (e) {
+        if (this.onerror) {
+          this.onerror(e);
+          if (this.ws) this.ws.close();
+        }
+      }
+    }
+
+    changePass(pass) {
+      this.pass = new RC4(pass);
+    }
+
+    close() {
+      this.ws.close();
+    }
+
+    requestWithID(id, module, func, ...params) {
+      const json = JSON.stringify({ id, module, function: func, params: params || [] }) + '\x00';
+      const buf = new TextEncoder('utf-8').encode(json);
+
+      if (this.pass) {
+        this.pass.crypt(buf);
+      }
+
+      const promise = new Promise((resolve, reject) => {
+        this.callbacks[id] = (data, errors) => {
+          if (errors && Array.isArray(errors) && errors.length > 0) {
+            reject(errors);
+          } else {
+            resolve(data);
+          }
+        };
+      });
+
+      this.ws.send(buf);
+      return promise;
+    }
+
+    request(module, func, ...params) {
+      if (++this.lastId > 2147483647) {
+        this.lastId = 1;
+      }
+      return this.requestWithID(this.lastId, module, func, ...params);
+    }
+  }
+
+  return SpiceAPI;
+})();
